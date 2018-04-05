6f456af5535c3aa067784111673e6f6b24b6b39c
Kacper Raczy <gfw.kra@gmail.com>
2018-04-05 22:47:33 +0200

    Fixed menu size on smaller devices. Updated font sizes.

--- OMatKo/SideMenu/MenuViewController.swift
+++ OMatKo/SideMenu/MenuViewController.swift
@@ -14,6 +14,22 @@ protocol MenuViewControllerDelegate: AnyObject {
 
 class MenuViewController: UIViewController {
     
+    // MARK: Init
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        setUp()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setUp()
+    }
+    
+    private func setUp() {
+        preferredContentSize = CGSize(width: 280, height: UIScreen.main.bounds.height)
+    }
+    
     // MARK: Outlets
     
     @IBOutlet weak var logoImageView: UIImageView!
--- OMatKo/SideMenu/SideMenuManager.swift
+++ OMatKo/SideMenu/SideMenuManager.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import ViewDeck
 
 class SideMenuManager: SideMenuControllerDataSource, MenuViewControllerDelegate {
     
